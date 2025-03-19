daa6dc341e504c65428bf90944d101c4fe27f4d1
Ravikant Tiwary <tiwariravikant04@gmail.com>
2025-03-19 07:43:50 +0530

    change to swiper lib

--- src/components/landing/Slider.tsx
+++ src/components/landing/Slider.tsx
@@ -1,11 +1,12 @@
-"use client"; 
+"use client";
 
 import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import { motion } from "framer-motion";
-
-const Slider = dynamic(() => import("infinite-react-carousel"), { ssr: false });
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface Service {
     title: string;
@@ -27,17 +28,19 @@ const InfiniteCarousel: React.FC<InfiniteCarouselProps> = ({ services, speed }) 
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
+    const settings = {
+        autoplay: true,
+        autoplaySpeed: speed,
+        arrows: true,
+        infinite: true,
+        slidesToShow: windowWidth < 768 ? 1 : 5,
+        slidesToScroll: windowWidth < 768 ? 1 : 5,
+    };
+
     return (
         <div className="w-full flex justify-center items-center py-10">
             <div className="w-[95%]">
-                <Slider
-                    autoplay
-                    autoplaySpeed={speed}
-                    arrows={true}
-                    infinite
-                    slidesToShow={windowWidth < 768 ? 1 : 5} // Show 1 slide on mobile, 5 on desktop
-                    slidesToScroll={windowWidth < 768 ? 1 : 5}
-                >
+                <Slider {...settings}>
                     {services.map((service, index) => (
                         <motion.div
                             key={index}
